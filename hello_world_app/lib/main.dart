41a7095446eb4da6280c5200db06fe35cef72444
Fiorella <fiorellaroma98@gmail.com>
2024-12-07 10:36:35 -0800

    Working with scaffold

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key}); //Inicializa el key del widget para el statelesswidget
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(child: Text('Hola Mundo')),
+    return const MaterialApp( //Se le pone const porque jamas va a cambiar
+    debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(child: Text('Hola Mundo')),
+      ) ,
     );
   }
-
 }
